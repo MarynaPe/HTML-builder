3aad41e5c1132ccee6e5376bd8fb46d3f4a180ad
MarynaPe <marynaperko@gmail.com>
2023-05-03 13:26:40 +0300

    feat: added solution for task 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+let data =""; 
+const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf8');
+readStream.on('data', function(chunk) { 
+  data += chunk; 
+  }).on('end', function() { 
+  console.log(data); 
+  }); 
